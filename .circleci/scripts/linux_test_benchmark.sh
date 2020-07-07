506212cba7c63284bb92638a9b54264a475170eb
Prashant Sail <psail4444@gmail.com>
2020-07-08 02:29:14 +0530

    updated benchmark scripts

--- .circleci/scripts/linux_test_benchmark.sh
+++ .circleci/scripts/linux_test_benchmark.sh
@@ -2,15 +2,16 @@
 
 # Hack needed to make it work with existing benchmark.py
 # benchmark.py expects jmeter to be present at a very specific location
-mkdir -p /home/ubuntu/.linuxbrew/Cellar/jmeter/5.3/libexec/bin/
-ln -s /opt/apache-jmeter-5.3/bin/jmeter /home/ubuntu/.linuxbrew/Cellar/jmeter/5.3/libexec/bin/jmeter
+mkdir -p /home/linuxbrew/.linuxbrew/Homebrew/Cellar/jmeter/5.3/libexec/
+ln -s /opt/apache-jmeter-5.3/lib/ /home/linuxbrew/.linuxbrew/Homebrew/Cellar/jmeter/5.3/libexec/lib
+ln -s /opt/apache-jmeter-5.3/bin/ /home/linuxbrew/.linuxbrew/Homebrew/Cellar/jmeter/5.3/libexec/bin
 
 # Start TS and redirect console ouptut and errors to a log file
-torchserve --start >> ts_console.log 2>&1
+torchserve --start --model-store=/tmp/ts_store > ts_console.log 2>&1
 sleep 30
 
 cd benchmarks
-python benchmark.py latency
+python benchmark.py latency --ts http://127.0.0.1:8080
 EXIT_CODE=$?
 
 torchserve --stop
