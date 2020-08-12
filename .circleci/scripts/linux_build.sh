194cacfaa6ea886511725777b52472e67963c09a
Prashant Sail <psail4444@gmail.com>
2020-08-12 16:20:09 +0530

    model archiver built once

--- .circleci/scripts/linux_build.sh
+++ .circleci/scripts/linux_build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+# Build torchserve
 python setup.py bdist_wheel --release --universal
+TS_BUILD_EXIT_CODE=$?
+
+# Build model archiver
+cd model-archiver/
+python setup.py bdist_wheel --release --universal
+MA_BUILD_EXIT_CODE=$?
+
+# If any one of the builds fail, exit with error
+if [ "$TS_BUILD_EXIT_CODE" -ne 0 ] || [ "$MA_BUILD_EXIT_CODE" -ne 0 ]
+then exit 1
+fi
